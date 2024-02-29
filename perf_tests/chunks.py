5834163e0264b98a384d0b8b0ebe392a9efa88a1
Alex Merose <al@merose.com>
2024-02-29 15:51:50 +0700

    Created tests to better understand chunks.

---
+++ perf_tests/chunks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import numpy as np
+import pandas as pd
+import xarray as xr
+import qarray as qr
+
+
+np.random.seed(42)
+temperature = 15 + 8 * np.random.randn(720, 1440, 240)
+precipitation = 10 * np.random.rand(720, 1440, 240)
+time = pd.date_range('2024-02-28', periods=240)
+reference_time = pd.Timestamp('2024-02-28')
+lat = np.linspace(-90, 90, num=720)
+lon = np.linspace(-180, 180, num=1440)
+
+ds = xr.Dataset(
+  data_vars=dict(
+    temperature=(['lat', 'lon', 'time'], temperature),
+    precipitation=(['lat', 'lon', 'time'], precipitation),
+  ),
+  coords=dict(
+    lat=lat,
+    lon=lon,
+    time=time,
+    reference_time=reference_time,
+  ),
+  attrs=dict(description='Random weather.')
+)
+
+df = qr.to_dd(ds, dict(time=48))
+df.compute()
+print(df.head())
+print(len(df))
